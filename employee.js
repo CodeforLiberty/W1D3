76f732445192cdd70aff62eb2e60a390b81e5019
Carlo Jesena <carlo.jesena@gmail.com>
2016-11-02 12:21:27 -0700

    first commit

---
+++ employee.js
@@ -0,0 +1,16 @@
+var employee = (function() {
+	this.name = getName();
+	this.age = getAge();
+  this.salary = getSalary();
+  function getName(name) {
+    return name;
+  }
+  function getAge(age) {
+    return age;
+  }
+  function getSalary(salary) {
+    return salary;
+  }
+  return {
+    
+  }());
