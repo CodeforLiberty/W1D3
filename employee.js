a0ef0c47ae6a1c68516935d7ba2d18f056776255
CodeforLiberty <sswell@live.com>
2016-11-02 16:03:37 -0400

    Solved problems

--- employee.js
+++ employee.js
@@ -1,16 +1,35 @@
+
+
 var employee = (function() {
 	this.name = getName();
 	this.age = getAge();
   this.salary = getSalary();
   function getName(name) {
-    return name;
+    return setName();
   }
   function getAge(age) {
-    return age;
+    return setAge();
   }
   function getSalary(salary) {
-    return salary;
+    return setSalary();
   }
   return {
+ setAge: function(age) {
+  return age;
+ },
+ setName: function (name) {
+  return name;
+ },
+ setSalary: function (salary) {
+  return salary;
+ },
+ increaseSalary: function (percentage) {
+  return getSalary() * percentage +"%"; 
+ },
+increaseAge: function () {
+  var setAge = getAge()+1;
+  return setAge;
+}
+  }
     
   }());
